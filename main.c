4ae286006ab8ab3647e3beb73df44417a1e7784b
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-02-17 14:19:30 +0300

    use custom shader programs

--- main.c
+++ main.c
@@ -1,22 +1,83 @@
 #define GL_SILENCE_DEPRECATION
 #include <GLFW/glfw3.h>
-#include <OpenGL/OpenGL.h>
+#include <OpenGL/gl.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+static unsigned int compile_shader(unsigned int type, const char *source) {
+  unsigned int id = glCreateShader(type);
+  glShaderSource(id, 1, &source, NULL);
+  glCompileShader(id);
+
+  int res;
+  glGetShaderiv(id, GL_COMPILE_STATUS, &res);
+
+  if (res == GL_FALSE) {
+    int len;
+    glGetShaderiv(id, GL_INFO_LOG_LENGTH, &len);
+    char *message = (char *)alloca(len * sizeof(char));
+    glGetShaderInfoLog(id, len, &len, message);
+    const char *vt = type == GL_VERTEX_SHADER ? "vertex" : "fragment";
+    fprintf(stderr, "[ERROR]: could not compile %s shader: %s\n", vt, message);
+    glDeleteShader(id);
+
+    return 0;
+  }
+
+  return id;
+}
+
+static unsigned int create_shader(const char *vert, const char *frag) {
+  unsigned int prog = glCreateProgram();
+  unsigned int vs = compile_shader(GL_VERTEX_SHADER, vert);
+  unsigned int fs = compile_shader(GL_FRAGMENT_SHADER, frag);
+
+  glAttachShader(prog, vs);
+  glAttachShader(prog, fs);
+  glLinkProgram(prog);
+  glValidateProgram(prog);
+
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  return prog;
+}
+
+int main(void) {
   glfwInit();
-  GLFWwindow *w = glfwCreateWindow(600, 600, "w", glfwGetPrimaryMonitor(), NULL);
+
+  GLFWwindow *w = glfwCreateWindow(800, 600, "w", NULL, NULL);
+
   glfwMakeContextCurrent(w);
+
+  printf("version = %s\n", glGetString(GL_VERSION));
 
   float pos[6] = {-0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f};
 
-  GLuint buff;
+  unsigned int buff;
   glGenBuffers(1, &buff);
   glBindBuffer(GL_ARRAY_BUFFER, buff);
   glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), pos, GL_STATIC_DRAW);
 
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+  const char *vs = "#version 120\n"
+                   "attribute vec4 position;\n"
+                   "\n"
+                   "void main() {\n"
+                   "  gl_Position = position;\n"
+                   "}\n";
+
+  const char *fs = "#version 120\n"
+                   "\n"
+                   "void main() {\n"
+                   "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color with full opacity\n"
+                   "}\n";
+
+  unsigned int shader = create_shader(vs, fs);
+
+  glUseProgram(shader);
 
   while (!glfwWindowShouldClose(w)) {
     glClear(GL_COLOR_BUFFER_BIT);
