c4ea3ba6d12086cc74f9670677a70c83a02de0d8
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-03-24 15:36:11 +0300

    modify minimum opengl version

--- main.c
+++ main.c
@@ -24,8 +24,8 @@ int main(void)
   glfwSetErrorCallback(error_callback);
   glfwInit();
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
