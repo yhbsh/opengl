9c04bcd598209ea57ed86b8bc049c5c61dccd333
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-03-24 14:35:20 +0300

    change bg

--- main.c
+++ main.c
@@ -132,6 +132,7 @@ int main(void) {
   glLinkProgram(program);
 
   while (!glfwWindowShouldClose(w)) {
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glUseProgram(program);
 
