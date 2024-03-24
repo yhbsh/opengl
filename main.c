eedd17d957c871b9890c6b7e418ee8694631098e
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-03-24 14:16:07 +0300

    fix

--- main.c
+++ main.c
@@ -80,13 +80,53 @@ int main(void) {
   glEnable(GL_DEPTH_TEST);
   glDepthFunc(GL_LESS);
 
+  float points[] = {0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f};
+  GLuint vbo = 0;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+  GLuint vao = 0;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+  const char *vertex_shader = "#version 400\n"
+                              "in vec3 vp;"
+                              "void main() {"
+                              "  gl_Position = vec4(vp, 1.0);"
+                              "}";
+
+  const char *fragment_shader = "#version 400\n"
+                                "out vec4 frag_colour;"
+                                "void main() {"
+                                "  frag_colour = vec4(1.0, 1.0, 1.0, 1.0);"
+                                "}";
+
+  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vs, 1, &vertex_shader, NULL);
+  glCompileShader(vs);
+  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fs, 1, &fragment_shader, NULL);
+  glCompileShader(fs);
+
+  GLuint program = glCreateProgram();
+  glAttachShader(program, fs);
+  glAttachShader(program, vs);
+  glLinkProgram(program);
+
   while (!glfwWindowShouldClose(w)) {
-    glClearColor(0.2f, 0.4f, 1.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    // wipe the drawing surface clear
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUseProgram(program);
+    glBindVertexArray(vao);
+    // draw points 0-3 from the currently bound VAO with current in-use shader
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    glfwSwapBuffers(w);
     glfwPollEvents();
+    glfwSwapBuffers(w);
   }
-
   return 0;
 }
