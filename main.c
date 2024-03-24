551e598fc1c8116fc7b38315b96d28feb4e3949c
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-03-24 14:10:27 +0300

    update to opengl 4.1

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #define GL_SILENCE_DEPRECATION
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <OpenGL/gl.h>
 #include <stdio.h>
@@ -28,46 +29,47 @@ static unsigned int create_shader(const char *vert, const char *frag) {
   return prog;
 }
 
+static void error_callback(int error, const char *description) { fprintf(stderr, "[ERROR]: %s\n", description); }
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+  if (key == GLFW_KEY_Q && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
+}
+
 int main(void) {
+  glfwSetErrorCallback(error_callback);
   glfwInit();
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   GLFWwindow *w = glfwCreateWindow(800, 600, "w", NULL, NULL);
 
   glfwMakeContextCurrent(w);
+  glfwSetKeyCallback(w, key_callback);
 
-  printf("version = %s\n", glGetString(GL_VERSION));
+  glewExperimental = GL_TRUE;
+  GLenum error = glewInit();
+  if (GLEW_OK != error) {
+    fprintf(stderr, "[ERROR]: %s\n", glewGetErrorString(error));
+    return 1;
+  }
 
-  float pos[6] = {-0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f};
+  const GLubyte *renderer = glGetString(GL_RENDERER);
+  const GLubyte *version = glGetString(GL_VERSION);
+  printf("Renderer: %s\n", renderer);
+  printf("OpenGL version supported %s\n", version);
 
-  unsigned int buff;
-  glGenBuffers(1, &buff);
-  glBindBuffer(GL_ARRAY_BUFFER, buff);
-  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), pos, GL_STATIC_DRAW);
-
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
-
-  const char *vs = "#version 120\n"
-                   "attribute vec4 position;\n"
-                   "\n"
-                   "void main() {\n"
-                   "  gl_Position = position;\n"
-                   "}\n";
-
-  const char *fs = "#version 120\n"
-                   "\n"
-                   "void main() {\n"
-                   "  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0); // Red color with full opacity\n"
-                   "}\n";
-
-  unsigned int shader = create_shader(vs, fs);
-
-  glUseProgram(shader);
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
 
   while (!glfwWindowShouldClose(w)) {
+    glClearColor(0.2f, 0.4f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(w);
     glfwPollEvents();
