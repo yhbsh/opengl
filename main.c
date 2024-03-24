161f3f25c2113e7ce63ee9a89b967d90bca89090
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-03-24 15:43:46 +0300

    add antialiasing

--- main.c
+++ main.c
@@ -24,6 +24,7 @@ int main(void)
   glfwSetErrorCallback(error_callback);
   glfwInit();
 
+  glfwWindowHint(GLFW_SAMPLES, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
