20b7861fb5f9bb818eb5a1d0068829d6f358a5a5
HoussemBousmaha <houssembousmaha.ksu@gmail.com>
2024-05-26 04:36:47 +0300

    add cube implementation

---
+++ src/cube.c
@@ -0,0 +1,74 @@
+#define GL_SILENCE_DEPRECATION
+#include <GL/glew.h>
+#include <OpenGL/gl.h>
+#include <GLFW/glfw3.h>
+
+#include <math.h>
+
+int main(void) {
+    glfwInit();
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Static Colorful Cube", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glEnable(GL_DEPTH_TEST);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0, 640.0 / 480.0, 0.1, 10.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0.0f, 0.0f, -3.0f);
+
+    while (!glfwWindowShouldClose(window)) {
+        float time = glfwGetTime();
+        float bounceAngle = fabs(sinf(time * 2.0f)) * 180.0f;
+        float bounceDistance = fabs(sinf(time * 2.0f)) * 2.0f - 1.0f;
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glLoadIdentity();
+        glTranslatef(0.0f, 0.0f, -5.0f + bounceDistance);
+        glRotatef(bounceAngle, 1.0f, 1.0f, 1.0f);
+
+        glBegin(GL_QUADS);
+            glColor3f(1.0f, 0.0f, 0.0f);
+            glVertex3f(-1.0f, -1.0f,  1.0f);
+            glVertex3f( 1.0f, -1.0f,  1.0f);
+            glVertex3f( 1.0f,  1.0f,  1.0f);
+            glVertex3f(-1.0f,  1.0f,  1.0f);
+
+            glColor3f(0.0f, 1.0f, 0.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f,  1.0f, -1.0f);
+            glVertex3f( 1.0f,  1.0f, -1.0f);
+            glVertex3f( 1.0f, -1.0f, -1.0f);
+
+            glColor3f(0.0f, 0.0f, 1.0f);
+            glVertex3f(-1.0f,  1.0f, -1.0f);
+            glVertex3f(-1.0f,  1.0f,  1.0f);
+            glVertex3f( 1.0f,  1.0f,  1.0f);
+            glVertex3f( 1.0f,  1.0f, -1.0f);
+
+            glColor3f(1.0f, 1.0f, 0.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f( 1.0f, -1.0f, -1.0f);
+            glVertex3f( 1.0f, -1.0f,  1.0f);
+            glVertex3f(-1.0f, -1.0f,  1.0f);
+
+            glColor3f(1.0f, 0.0f, 1.0f);
+            glVertex3f( 1.0f, -1.0f, -1.0f);
+            glVertex3f( 1.0f,  1.0f, -1.0f);
+            glVertex3f( 1.0f,  1.0f,  1.0f);
+            glVertex3f( 1.0f, -1.0f,  1.0f);
+
+            glColor3f(0.0f, 1.0f, 1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f,  1.0f);
+            glVertex3f(-1.0f,   1.0f,  1.0f);
+            glVertex3f(-1.0f,   1.0f, -1.0f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
+}
